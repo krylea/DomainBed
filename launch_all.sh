9722a0b04d79640711eaec5b3223cd01c4a5a490
Kira Selby <kaselby@uwaterloo.ca>
2022-01-03 19:36:03 -0500

    scripts

---
+++ launch_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dataset = "VLCS"
+data_dir="VLCS"
+algorithm="ERM"
+num_envs=4
+
+num_runs=3
+
+for (( i = 0 ; i < $num_envs ; i++ ))
+do
+    for (( j = 0 ; j < $num_runs ; j++ ))
+    do
+        sbatch train.sh $dataset $data_dir $i $algorithm $j
+    done
+done
+
+
